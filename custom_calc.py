b768237b8be92c904015edeb0333eaf76f7d67fc
Fabian Burth <47922941+RingTuEnigma@users.noreply.github.com>
2020-02-11 17:22:48 +0100

    Commented, delete useless print functions and added a message for running and stopping the server

--- custom_calc.py
+++ custom_calc.py
@@ -1,18 +1,19 @@
 import json
 from Heinrich_V1 import Call
 
-def test_function(body):
+#def test_function(body):
   #Prints the content of the HTTP POST Request Body
-  print(body)
+  #print(body)
   #The JSON encoded content is decoded
-  data_list = json.loads(body)
+  #data_list = json.loads(body)
   #response_data = call(classes,absolute,z)
-  print(data_list)
+  #print(data_list)
   #Shows that the content is a list and not just a string
-  data_JSON = json.dumps(data_list) #exchange data_list with response_data
-  print(data_JSON)
-  return data_JSON
+  #data_JSON = json.dumps(data_list) #exchange data_list with response_data
+  #print(data_JSON)
+  #return data_JSON
 
+#De- and encoding the input and output data and calling the calculation module
 def do_calculation(input_JSON):
   input_data = json.loads(input_JSON)
   output_data = Call(input_data['classes'], input_data['absolute'], input_data['z'])
--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import custom_calc
 
-class calculationHandler(BaseHTTPRequestHandler):
+#Subclass to handle the HTTP Request
+class CalculationHandler(BaseHTTPRequestHandler):
+  #Handling the CORS-Policy of the browser
   def do_OPTIONS(self):
     self.send_response(200, "ok")
     self.send_header('Access-Control-Allow-Origin', '*')
@@ -16,10 +18,12 @@ class calculationHandler(BaseHTTPRequestHandler):
     #self.end_headers()
     #self.wfile.write('Hello Test'.encode())
 
+  #Handling the actual request with the client data
   def do_POST(self):
+    #Reading the entity-body of the HTTP-Request
     content_len = int(self.headers.get('Content-Length'))
     post_body = self.rfile.read(content_len)
-    custom_calc.test_function(post_body)
+    #custom_calc.test_function(post_body)
     response_data = custom_calc.do_calculation(post_body)
     self.send_response(200)
     self.send_header('content-type', 'json/application')
@@ -27,13 +31,16 @@ class calculationHandler(BaseHTTPRequestHandler):
     self.end_headers()
     self.wfile.write(response_data.encode())
 
-def run(server_class=HTTPServer, handler_class=calculationHandler):
+#Running the HTTP-Server and passing our request handler class
+def run(server_class=HTTPServer, handler_class=CalculationHandler):
     server_address = ('127.0.0.1', 8000)
-    httpd = server_class(server_address, handler_class)
-    httpd.serve_forever()
+    try:
+      httpd = server_class(server_address, handler_class)
+      print("Server will be running now!")
+      httpd.serve_forever()
+    except:
+      print("You stopped the server or something went wrong!")
 
-#Clever trick in a lot of python scripts, figure it out yourself ;)
+#Making sure it doesnt run if it is imported as a module
 if __name__ == '__main__':
   run()
-
-#TEST
